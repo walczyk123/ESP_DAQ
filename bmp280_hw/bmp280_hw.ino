4a6181fab22ef811e9066467a4120c8efef069ef
walczyk123 <58117001+walczyk123@users.noreply.github.com>
2019-12-20 14:15:35 +0100

    Nowa wersja
    -lokalne biblioteki

--- bmp280_hw/bmp280_hw.ino
+++ bmp280_hw/bmp280_hw.ino
@@ -5,11 +5,11 @@
  wysyłanie danych do clienta
 **********************************************/
 
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
-#include <ESP8266WiFi.h>
+#include "libs/Wire/Wire.h"
+#include "libs/SPI/SPI.h"
+#include "libs/Adafruit_Sensor.h"
+#include "libs/Adafruit_BMP280.h"
+#include "libs/ESP8266WiFi.h"
 
 #define BMP_SCK 13
 #define BMP_MISO 12
